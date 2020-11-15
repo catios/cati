d725d1ec1c0d490307326d05bfa56c560586ef37
parsa mpsh <parsampsh@gmail.com>
2020-11-15 16:44:19 +0330

    frontend sysarch: consider `i686`, `i586` and `i486` as `i386`

--- src/frontend/SysArch.py
+++ src/frontend/SysArch.py
@@ -36,6 +36,6 @@ def sys_arch():
         return 'i386'
     else:
         arch = os.uname().machine
-        if arch == 'i686':
+        if arch in ['i686', 'i586', 'i486']:
             arch = 'i386'
         return arch
