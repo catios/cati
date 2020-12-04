743378d8ae818332735a59f93e7ddfe4eaed2b33
parsa mpsh <parsampsh@gmail.com>
2020-12-04 20:54:18 +0330

    tests: fix a bug

--- tests/items/test_install.py
+++ tests/items/test_install.py
@@ -35,49 +35,46 @@ class test_install(TestCore):
         repo1 = "file://" + os.getcwd() + '/repository name=test arch=all pkg=cati'
         repo2 = "file://" + os.getcwd() + '/repository name=test arch=i386 pkg=cati'
 
+        self.assert_equals(self.run_command('repo', ['--scan', os.getcwd() + '/repository']), 0)
+
         self.assert_equals(self.run_command('repo', ['--add', repo1]), 0)
         self.assert_equals(self.run_command('repo', ['--add', repo2]), 0)
         self.assert_equals(self.run_command('update'), 0)
 
-        self.disable_raising()
-        try:
-            self.assert_equals(self.run_command('install', ['testpkg10', '-y']), 0)
+        self.assert_equals(self.run_command('install', ['testpkg10', '-y']), 0)
 
-            self.assert_true(Pkg.is_installed('testpkg10'))
-            self.assert_true(Pkg.is_installed('testpkg11'))
+        self.assert_true(Pkg.is_installed('testpkg10'))
+        self.assert_true(Pkg.is_installed('testpkg11'))
 
-            self.assert_equals(self.run_command('install', ['testpkgb', '-y']), 0)
+        self.assert_equals(self.run_command('install', ['testpkgb', '-y']), 0)
 
-            self.assert_true(Pkg.is_installed('testpkgb'))
-            self.assert_true(Pkg.is_installed('testpkgc'))
-            self.assert_true(not Pkg.is_installed_manual('testpkgc'))
+        self.assert_true(Pkg.is_installed('testpkgb'))
+        self.assert_true(Pkg.is_installed('testpkgc'))
+        self.assert_true(not Pkg.is_installed_manual('testpkgc'))
 
-            self.assert_equals(self.run_command('install', ['testpkgz', '-y']), 0)
+        self.assert_equals(self.run_command('install', ['testpkgz', '-y']), 0)
 
-            self.assert_true(not Pkg.is_installed('testpkgb'))
-            self.assert_true(not Pkg.is_installed('testpkgc'))
-            self.assert_true(Pkg.is_installed('testpkgz'))
+        self.assert_true(not Pkg.is_installed('testpkgb'))
+        self.assert_true(not Pkg.is_installed('testpkgc'))
+        self.assert_true(Pkg.is_installed('testpkgz'))
 
-            self.assert_equals(self.run_command('install', ['testpkgb', '-y']), 0)
+        self.assert_equals(self.run_command('install', ['testpkgb', '-y']), 0)
 
-            self.assert_true(Pkg.is_installed('testpkgb'))
-            self.assert_true(Pkg.is_installed('testpkgc'))
-            self.assert_true(not Pkg.is_installed('testpkgz'))
+        self.assert_true(Pkg.is_installed('testpkgb'))
+        self.assert_true(Pkg.is_installed('testpkgc'))
+        self.assert_true(not Pkg.is_installed('testpkgz'))
 
-            self.assert_equals(self.run_command('remove', ['testpkgb', 'testpkgc', '-y']), 0)
+        self.assert_equals(self.run_command('remove', ['testpkgb', 'testpkgc', '-y']), 0)
 
         # test upgrade
-            self.assert_equals(self.run_command('install', ['testpkgc=1.0', '-y']), 0)
+        self.assert_equals(self.run_command('install', ['testpkgc=1.0', '-y']), 0)
 
-            testpkgc = Pkg.load_last('testpkgc')
-            self.assert_equals(testpkgc.installed(), '1.0')
+        testpkgc = Pkg.load_last('testpkgc')
+        self.assert_equals(testpkgc.installed(), '1.0')
 
-            self.assert_equals(self.run_command('upgrade', ['-y']), 0)
+        self.assert_equals(self.run_command('upgrade', ['-y']), 0)
 
-            testpkgc = Pkg.load_last('testpkgc')
-            self.assert_equals(testpkgc.installed(), '2.0')
-        except:
-            self.enable_raising()
-            print('faild. ignoring...', end='')
+        testpkgc = Pkg.load_last('testpkgc')
+        self.assert_equals(testpkgc.installed(), '2.0')
 
         self.refresh_env()
